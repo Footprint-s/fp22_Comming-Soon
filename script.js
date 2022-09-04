9fd617a40be75b337235b5bc0c5973b9d776d324
aman162000 <borseaman16@gmail.com>
2022-09-04 18:59:15 +0530

    Add newsletter feature

---
+++ script.js
@@ -0,0 +1,41 @@
+// add "send email" button DOM and set id="send" in index file
+sendBtn = document.getElementById("send");
+const email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const sendRequest = (method, url, data) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open(method, url);
+    if (data) {
+      xhttp.setRequestHeader("Content-Type", "application/json");
+    }
+    xhttp.onloadend = () => {
+      if (xhttp.status >= 400) {
+        reject(JSON.parse(xhttp.response));
+      } else {
+        resolve(JSON.parse(xhttp.response));
+      }
+    };
+
+    xhttp.send(JSON.stringify(data));
+  });
+
+  return promise;
+};
+
+sendBtn.addEventListener("click", () => {
+  let email = document.getElementById("email-textbox").value;
+  if (email.match(email_regex)) {
+    sendRequest("POST", "https://api.msufp.in/api/newsletter/", {
+      email: email,
+    })
+      .then((resp) => {
+        alert(resp.message ? resp.message : resp.email);
+      })
+      .catch((err) => {
+        alert(`Error:${err}`);
+      });
+  } else {
+    alert("E-mail address is not valid.");
+  }
+});
